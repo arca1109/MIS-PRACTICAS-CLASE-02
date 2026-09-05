18ea5d07739b24e49afece474296acb2a8e08904
arca1109 <gcastillolaboral@gmail.com>
2026-09-05 10:57:33 -0400

    IMC

---
+++ masacorporal.py
@@ -0,0 +1,5 @@
+print("Hola, conoce u índice de masa corporal")
+peso = float(input("Introduce tu peso en kilogramos: "))
+altura = float(input("Introduce tu altura en metros: "))
+imc = peso / (altura ** 2)
+print(f"Tu índice de masa corporal es: {imc:.2f} kg/m²")
