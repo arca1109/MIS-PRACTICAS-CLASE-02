1f4c1783cc32dd8e3c4668fb7c9622c53db69b75
arca1109 <gcastillolaboral@gmail.com>
2026-09-05 00:58:58 -0400

    áreadetriángulo

---
+++ áreadetriángulo.py
@@ -0,0 +1,5 @@
+print("Calculadora de área de triángulo")
+base = float(input("Introduzca la base del triángulo: "))    
+altura = float(input("Introduzca la altura del triángulo: "))
+area = (base * altura) / 2
+print("El área del triángulo es:", area)
