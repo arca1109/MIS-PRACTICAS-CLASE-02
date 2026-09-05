1ef228774207e39b83634e3442bc6ba17ed0395b
arca1109 <gcastillolaboral@gmail.com>
2026-09-05 02:20:28 -0400

    paroimapar

---
+++ paroimpar.py
@@ -0,0 +1,6 @@
+print("Determina si un número es par o impar")
+numero = int(input("Introduce un número: "))
+if numero % 2 == 0:
+    print(f"El número {numero} es par.")
+else:
+    print(f"El número {numero} es impar.")
