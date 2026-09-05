da4187baa841498af67305e21177beedeee65caf
arca1109 <gcastillolaboral@gmail.com>
2026-09-05 02:13:19 -0400

    convertidordetemperatura

---
+++ convertidordetemperatura.py
@@ -0,0 +1,6 @@
+print("Convertidor de temperatura")
+
+celsius = int(input("Introduce la temperatura en grados Celsius: "))
+fahrenheit = (celsius * 9/5) + 32
+
+print(f"{celsius} grados celsius equivalen a {fahrenheit} grados Fahrenheit")
